b78e82893aed363d06c4ac0e29e599fc41c0518f
Marcelo <subzeroxp@gmail.com>
2018-05-06 05:44:41 -0300

    Commit no win

--- Curso Dev Web/js/script.js
+++ Curso Dev Web/js/script.js
@@ -1,2 +1,8 @@
-//alert ("Alerta");
-console.log ("Confirmar?")
+//alert (somar)
+//console.log ("calcular?")
+//prompt (Somar números: num1)
+document.querySelector("#calcular").addEventListener("click", function (somar) {
+    return "#valorB" + "#valorA";
+    alert (somar)
+
+} ) 
